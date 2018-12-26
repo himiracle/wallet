77d808c064e0013789d6bdf5966ffc9fee117d9c
blockchain1124 <blockchain1124@gmail.com>
2018-12-26 15:04:48 +0900

    start

---
+++ main.js
@@ -0,0 +1,39 @@
+const electron = require("electron"),
+  path = require("path"),
+  url = require("url");
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+    title: "MyCoin Wallet"
+  });
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file",
+      slashes: true
+    })
+  );
+
+  mainWindow.on("close", () => {
+    mainWindow = null;
+  });
+};
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+app.on("ready", createWindow);
